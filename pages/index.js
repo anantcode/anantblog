7030f35a1e63d6a4a570be4cdee8b0c669bf2def
Anant <anantcode@gmail.com>
2022-04-05 21:45:19 +0530

    Add getStaticProps() to load the posts(data) from MD file.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import Head from "next/head";
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log(posts);
   return (
     <div>
       <Head>
         <title>Dev Blog</title>
       </Head>
-
       <h2>Hello Anant!!</h2>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: { posts: "The Posts" }, //test
+  };
 }
