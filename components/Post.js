e3dfa4eac58778db72bfd6781a49a8ca0faaf50c
Anant <anantcode@gmail.com>
2022-04-06 11:03:45 +0530

    Update Post component to show the data from prop (post)

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,9 @@
-import Link from "next/link";
+import Link from "next/link"; //because we want link to single post from this component
 
-export default function Post() {
-  return <div>Post</div>;
+export default function Post({ post }) {
+  return (
+    <div class="card">
+      <img src={post.frontmatter.cover_image} />
+    </div>
+  );
 }
