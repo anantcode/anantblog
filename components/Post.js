4393dbdc86a8fbe5738aeaaa2ed7493705d2992e
Anant <anantcode@gmail.com>
2022-04-06 12:45:33 +0530

    resize image (could have used css but meh)

--- components/Post.js
+++ components/Post.js
@@ -3,7 +3,7 @@ import Link from "next/link"; //because we want link to single post from this co
 export default function Post({ post }) {
   return (
     <div class="card">
-      <img src={post.frontmatter.cover_image} />
+      <img src={post.frontmatter.cover_image} width={250} height={250} />
       <div className="post-date">Posted on {post.frontmatter.date}</div>
       <h3>{post.frontmatter.title}</h3>
       <p>{post.frontmatter.excerpt}</p>
